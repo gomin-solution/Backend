bfd71bdde6b82b28b7b2102d29ac4f1a5f19c852
Lsp <spyro9282@gmail.com>
2022-12-07 00:57:27 +0900

    [chore]쪽지방 날짜 확인

--- src/services/note.service.js
+++ src/services/note.service.js
@@ -29,15 +29,18 @@ class NoteService {
 
     const rooms = allRooms.map((room) => {
       let nickname;
-      let recentDate;
+
       room.User1.userKey == userKey
         ? (nickname = room.User2.nickname)
         : (nickname = room.User1.nickname);
 
-      room.Notes.length
-        ? (recentDate = room.Notes.createdAt)
-        : (recentDate = room.createdAt);
+      const recentDate = room.Notes.createdAt;
+      console.log("//////////recentDate/////////////");
+      console.log(recentDate);
+
       const date = dayjs(recentDate).tz().format("YYYY/MM/DD HH:mm");
+      console.log("///////////date///////////");
+      console.log(date);
       return {
         roomId: room.roomId,
         title: room.title,
