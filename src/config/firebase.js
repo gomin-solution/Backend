d55fd9ef92fed849adad4556a160dc34203def07
Lsp <spyro9282@gmail.com>
2022-12-11 03:16:27 +0900

    [docs]firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,16 +1,17 @@
 require("dotenv").config();
+const ENV = process.env;
 
 module.exports = {
   type: "service_account",
-  project_id: "gomin-96695",
-  private_key_id: "bbf235a6ec9504356327c5ce1b20dabbd9f6b1a7",
-  private_key:
-    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC2nmfkGYRxkrR3\nn2AD1TP/5sJ8+YVefNy2t2UfiDNOV4/ZDz6QEwhhRmdBYjVi1aBJX35MWH5QA2L4\nUYTsZ+ZUEC4pQsjibRHduh33mA24HrrmmFpWr//FOJjlpQUBROFfFmRwHUIxajmv\nNyVRe9bKkVF4rI+9mR2f85M1lZkwTdfMtQE/CDlJnbm2H12DUUlV4Ry+yTYAK7pz\ni+cjOsXcTNkXN1nWmPQykUh1OtqF7RCzorQRCJhFZUUVggz1AYn3z948jiEQo6X8\ng2B+rm55G/7WeuLJdJ0eTaiKGTLtoRjMpRAFE8UUVY2aro/p15EZXqzwi3c5CI90\nsyQA3xzFAgMBAAECggEAHjEUNgbt+aTqhzhnNFsETNOG5giOvy9fginR4yOub85U\nLuuxSTqMUSDFo26sKtnstRS/mbmrsGjO7QT1N+x5jP5b02CCO8UShhjmKXAPHJig\nB826aciRPbb8uELPEAWmSQU3t71umyLXxJsWZWFXAoiB8rR80zjESBSKCvrfp7d/\nbiy/GmHcmadYVcQnApWMR6L+fR2m8HyBkRBDyCQxwFfJOTePvdVi/uaxOFPqv/pz\nerr+0ylMAr5XqGOf/G0croJPiavIJBMI88B5ljpTnfqW2LVf0g0x5b017Yz1iW5k\nNhbtdH8si8ThXATN9NTG3eNykZ72grFNfeb0KC6EgQKBgQDZg78X6V1wjixV376D\nV7ZxKTazCQBf6sS3o/ZEfK1yn1mUC8yuFvs6tgICil8f6e9Cm+SleTMltDmx4xhl\ndjt2C0TJVaMGqBT12hMUUK2pSdXMxxIrbiTiFs2QdWbTqDItpEk4tfUjfGvOJVUv\ng4Xx6CHwzXCrWfw6mAUBW48RYQKBgQDW7hAVcYY7vKYyaCVvbd2kPNgCpVw+oWH0\nGfFuMFKLGoNJjqulgGkWW6eJCqg+W1G3wsScuvDZ1x8vVNStJBEddGZE+9IiO38L\n1iL1PSZpdPgoxIeJgo5F1xwEc8x0VmAo+n7F0KD1Hd5tbWgSvvNVRVwyCO/W0ZvW\nng7K5DEx5QKBgD3aJODbz2JOS3Uv5rDapFqRaaiHir+7tqyxxzm6amgJwCNFJ7Lp\nfg836mJmjp44m3zJb79OM6kpEq7u8OUJNedfVPQbkfw2l59Bn+NK/dcB0/YjNZt7\n+qj/6vyFJfRSA9LOwLOLqmWo388C9rJIKDKd2+jpwnw3Xm4bBHFaY9iBAoGAWIOm\nYA5mx8dDctLtR9ypQPBFzaEoQirYWl0szsJb3CnJE8wL6JbfsHOCpa+b5kJZfyJZ\nMmrSsgQkNavsAnouCkFbtOE4gxyZj9hagUDzxs6DPNbjMsBvs1eJlFPUSb0g0P+I\nohLvlsL8TXiXOdEmCWINPBXuIDwmv9ZMJUag8/kCgYEA1kDxoG2MYn3ci8V5JCZC\nRuodd0NRxMm82enZzQUssu6erFr6hLE6WVsvIP1ephSwBvaMBYWZ9eoJe6UyQ0cQ\n1C2g0AOPE8iqnMPFFKpanW7VA5clmEEIciT5zD1grMZ4E3ajcULg0PAnICWkjTh9\nNqV3JvRs5dTot5r841x0+uQ=\n-----END PRIVATE KEY-----\n",
-  client_email: "firebase-adminsdk-fkb0x@gomin-96695.iam.gserviceaccount.com",
-  client_id: "107650074481053111273",
+  project_id: ENV.PROJECT_ID,
+  private_key_id: ENV.PRIVATE_KEY_ID,
+  private_key: ENV.PRIVATE_KEY,
+
+  client_email: ENV.CLIENT_EMAIL,
+  client_id: ENV.CLIENT_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   client_x509_cert_url:
-    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fkb0x%40gomin-96695.iam.gserviceaccount.com",
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-urqoe%40gomin-9afcf.iam.gserviceaccount.com",
 };
