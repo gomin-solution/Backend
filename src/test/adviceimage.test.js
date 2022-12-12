9cd8fa4bbf979d89206ed3fd507c3009858f0a90
leejun <koreadlwns@naver.com>
2022-12-13 00:10:29 +0900

    [feat] AdviceImage test

--- src/test/adviceimage.test.js
+++ src/test/adviceimage.test.js
@@ -11,12 +11,16 @@ const data = {
 
 describe("advice 이미지 테스트", () => {
     test("image가 등록되는가?", async () => {
-
         adviceImageService.adviceImageRepository.createAdviceImage = jest.fn(() => data);
         expect( // 다음과 같은 결과가 나와야한다.
           await adviceImageService.createAdviceImage(adviceId, imageUrl)
         ).toEqual(
           data);
+    });
     
-      });
+    test("adviceImageDelete가 성공하는가?", async () => {
+        adviceImageService.adviceImageRepository.imageDelete = jest.fn(() => data);
+        adviceImageService.adviceImageRepository.imageDelete = jest.fn();
+        expect(await adviceImageService.imageDelete(adviceId)).toEqual();
+    });
 })
